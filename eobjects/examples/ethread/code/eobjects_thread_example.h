08c77fd87ddef627b4dcfa4c7ff5340d7ca7dee9
pekkalehtikoski <pekka.lehtikoski@gmail.com>
2017-01-03 21:59:12 -0500

    ...

---
+++ eobjects/examples/ethread/code/eobjects_thread_example.h
@@ -0,0 +1,20 @@
+/**
+
+  @file    eobjects_thread_example.h
+  @brief   Example code about naming objects.
+  @author  Pekka Lehtikoski
+  @version 1.0
+  @date    28.12.2016
+
+  This example demonstrates how to create threads.
+
+  Copyright 2012 Pekka Lehtikoski. This file is part of the eobjects project and shall only be used, 
+  modified, and distributed under the terms of the project licensing. By continuing to use, modify,
+  or distribute this file you indicate that you have read the license and understand and accept 
+  it fully.
+
+****************************************************************************************************
+*/
+
+void thread_example_1();
+void thread_example_2();
